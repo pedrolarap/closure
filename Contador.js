642ad769ab87a4b212260e792e10bce80ad2d33f
pedrolarap <pedrolarapeguero@gmail.com>
2023-03-23 17:41:20 -0400

    first commit

---
+++ Contador.js
@@ -0,0 +1,14 @@
+ 
+ 
+ function contador(valor){
+
+    return ()=>++valor
+ }
+
+ const valor2 = contador(1);
+ console.log(valor2())
+ console.log(valor2())
+ console.log(valor2())
+ console.log(valor2())
+ console.log(valor2())
+ 
